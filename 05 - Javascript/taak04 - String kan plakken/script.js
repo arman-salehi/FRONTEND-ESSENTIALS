0c2db3385eb9e74cf705e508116d8539a4ba664d
Arman Salehi <arman.salehi@student.rocva.nl>
2020-10-27 11:28:20 +0100

    string en console toegepast

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,11 @@
+alert("Deze website maakt geen gebruik van cookies. Dit is puur voor het idee. Even mee spelen a.u.b :)");
+
+let mijnVoornaam = "Arman";
+let mijnAchternaam = "Salehi";
+let mijnLeeftijd = "17";
+
+let volledigeZin = "Mijn naam is" + " " + mijnVoornaam + " " + mijnAchternaam + " " + "en ik ben" + " " + mijnLeeftijd + " " + "jaar";
+
+
+console.log(volledigeZin);
+
