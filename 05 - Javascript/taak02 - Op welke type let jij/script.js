3c47b0416c87141027a94b025e28ee9dbb07de4e
Arman Salehi <arman.salehi@student.rocva.nl>
2020-10-27 10:52:05 +0100

    JS toegepast console gecheckt

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,9 @@
+alert("Deze website maakt geen gebruik van cookies. Dit is puur voor het idee. Even mee spelen a.u.b :)");
+
+let mijnVoornaam = "Arman Salehi"
+let mijnAchterenaam = "Salehi"
+let mijnLeeftijd = "16"
+let ikBenStemGerechtig = "nah"
+let ikMagLegaalAlcoholKopen = "naah"
+let ikMagBrommerRijden = "yes"
+let SoftwareOntwikkeling = "yess"
