46e3cb3832973f6140bc9def8e9e971e5558168d
Arman Salehi <arman.salehi@student.rocva.nl>
2020-10-27 11:04:00 +0100

    object en console log toegepast

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,12 @@
+alert("Deze website maakt geen gebruik van cookies. Dit is puur voor het idee. Even mee spelen a.u.b :)");
+
+let student = {
+    let, mijnVoornaam: "Arman Salehi",
+    let, mijnAchterenaam: "Salehi",
+    let, mijnLeeftijd: "16",
+    let, ikBenStemGerechtig: "nah",
+    let, ikMagLegaalAlcoholKopen: "naah",
+    let, ikMagBrommerRijden: "yes",
+    let, SoftwareOntwikkeling: "yess",
+};
+console.log(student.leeftijd);
