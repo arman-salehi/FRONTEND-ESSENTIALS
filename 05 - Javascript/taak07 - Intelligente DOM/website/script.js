d3c5921fd6ceff9e9882a932b750cdcee15ee8e3
Arman Salehi <arman.salehi@student.rocva.nl>
2020-10-28 17:12:11 +0100

    achtergrond kleuren en text kleuren aangepast

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -6,4 +6,14 @@ console.log(window.document);
 // document.querySelector("#posterimg1").src = null;
 
 
-// document.querySelector("#poster1");
+var d1 = document.querySelector("#poster1");
+d1.style.backgroundColor = 'purple';
+d1.style.color = 'white';
+
+var d2 = document.querySelector("#poster2");
+d2.style.backgroundColor = 'violet';
+d2.style.color = 'white';
+
+var d3 = document.querySelector("#poster3");
+d3.style.backgroundColor = 'pink';
+d3.style.color = 'white';
